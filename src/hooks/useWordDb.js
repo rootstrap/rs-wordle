5edd6ed4dbae4c6d23f8f644196851dbdda7ad50
Manuela Viola <manuela.viola@rootstrap.com>
2022-11-17 15:09:25 +0100

    feat: use random word of 5 or 6 characters

--- src/hooks/useWordDb.js
+++ src/hooks/useWordDb.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
 import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { initialize } from '@paunovic/random-words';
 
 import { WORDS_COLLECTION } from 'firebase/collections';
 import firebaseData from 'firebase/firebase';
+import { getRandomInt } from 'utils/helpers';
+
+const ADMITED_WORDS_SIZES = [5, 6];
+const DEFAULT_WORDS = ['GENIE', 'GRACE', 'SPACE', 'CLASS', 'NOTICE', 'WORDS'];
 
 const useWordDb = () => {
   const [word, setWord] = useState('');
   const [wordDate, setWordDate] = useState(null);
+
+  const RANDOM = initialize({ countryCode: 'us' });
 
   const today = new Date().toLocaleDateString().split('/').join('');
   const { firebaseDb } = firebaseData;
@@ -15,6 +22,7 @@ const useWordDb = () => {
   useEffect(() => {
     const getWord = async () => {
       if (wordDate !== today) {
+        setWordDate(today);
         try {
           const docRef = doc(firebaseDb, WORDS_COLLECTION, today);
           const docSnap = await getDoc(docRef);
@@ -22,18 +30,31 @@ const useWordDb = () => {
             const { word: todaysWord } = docSnap.data();
             setWord(todaysWord);
           } else {
-            await setDoc(doc(wordsRef, today), { word: 'GRACE' });
-            setWord('GRACE');
+            let todaysWord = '';
+            const wordsCollection = RANDOM.words(100);
+            const correctSizeWords = await wordsCollection.filter(word =>
+              ADMITED_WORDS_SIZES.includes(word.length)
+            );
+            if (!!correctSizeWords.length) {
+              const randomIndex = getRandomInt(correctSizeWords.length);
+              todaysWord = correctSizeWords[randomIndex];
+            } else {
+              const randomIndex = getRandomInt(DEFAULT_WORDS.length);
+              todaysWord = DEFAULT_WORDS[randomIndex];
+            }
+            await setDoc(doc(wordsRef, today), { word: todaysWord });
+            setWord(todaysWord);
           }
-          setWordDate(today);
         } catch (err) {
           console.error(err);
-          alert(err.message);
+          const randomIndex = getRandomInt(DEFAULT_WORDS.length);
+          const todaysWord = DEFAULT_WORDS[randomIndex];
+          setWord(todaysWord);
         }
       }
     };
     getWord();
-  });
+  }, [RANDOM, firebaseDb, today, wordDate, wordsRef]);
 
   return {
     word,

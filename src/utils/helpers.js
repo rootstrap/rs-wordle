71832caded30e6dc4e2683382202ecb565c34f94
Manuela Viola <manuela.viola@rootstrap.com>
2022-11-18 13:25:53 +0100

    fix: unify date to us language

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,3 @@
 export const getRandomInt = max => Math.floor(Math.random() * max);
 
-export const getTodaysDate = () => new Date().toLocaleDateString().split('/').join('');
+export const getTodaysDate = () => new Date().toLocaleDateString('en-US').split('/').join('');

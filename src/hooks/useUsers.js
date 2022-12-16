1177dfde85525919cf1e7622c9f86fc362965703
Manuela Viola <manuela.viola@rootstrap.com>
2022-12-16 14:36:53 -0300

    fix: remove unnecessary filterChanged state

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 
 import { USERS } from 'firebase/collections';
@@ -8,40 +8,40 @@ const { firebaseDb } = firebaseData;
 
 const useUsers = () => {
   const [filters, setFilters] = useState({ username: '' });
-  const [filtersChanged, setFiltersChanged] = useState(false);
   const [usersList, setUsersList] = useState();
 
   const { username } = filters;
 
-  const changeFilter = (key, value) => {
+  const getUsers = useCallback(async () => {
+    try {
+      const q = query(collection(firebaseDb, USERS), orderBy('name'));
+      const docs = await getDocs(q);
+      const newUsersList = [];
+      docs.forEach(doc => {
+        const { email, name, photo, uid } = doc.data();
+        if (name.toUpperCase().includes(username.toUpperCase())) {
+          newUsersList.push({ email, name, photo, id: uid });
+        }
+      });
+      setUsersList(newUsersList);
+    } catch (err) {
+      console.log('err: ', err);
+    }
+  }, [username]);
+
+  const changeFilter = async (key, value) => {
     setFilters({
       ...filters,
       [key]: value,
     });
-    setFiltersChanged(true);
+    await getUsers();
   };
 
   useEffect(() => {
     (async function () {
-      if (!usersList || filtersChanged) {
-        setFiltersChanged(false);
-        try {
-          const q = query(collection(firebaseDb, USERS), orderBy('name'));
-          const docs = await getDocs(q);
-          const newUsersList = [];
-          docs.forEach(doc => {
-            const { email, name, photo, uid } = doc.data();
-            if (name.toUpperCase().includes(username.toUpperCase())) {
-              newUsersList.push({ email, name, photo, id: uid });
-            }
-          });
-          setUsersList(newUsersList);
-        } catch (err) {
-          console.log('err: ', err);
-        }
-      }
+      await getUsers();
     })();
-  }, [filtersChanged, username, usersList]);
+  }, [getUsers]);
 
   return {
     filters,

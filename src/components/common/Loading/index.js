cd4cd67ba25e4abb0e8ddd437ba7cda678c3b825
Manuela Viola <manuela.viola@rootstrap.com>
2022-12-21 13:27:56 -0300

    fix: code review comments

--- src/components/common/Loading/index.js
+++ src/components/common/Loading/index.js
@@ -6,65 +6,65 @@ import { LETTER_STATUS } from 'constants/types';
 import './styles.css';
 import LetterInput from '../LetterInput';
 
+const styles = {
+  container: { height: '50px', width: '50px' },
+  correct: { backgroundColor: LETTER_STATUS.correct },
+  incorrect: { backgroundColor: LETTER_STATUS.incorrect },
+  misplaced: { backgroundColor: LETTER_STATUS.misplaced },
+};
+
+const loadingWords = [
+  {
+    back: 'L',
+    front: 'R',
+    backStyle: styles.correct,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'O',
+    front: 'S',
+    backStyle: styles.incorrect,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'A',
+    front: 'W',
+    backStyle: styles.misplaced,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'D',
+    front: 'O',
+    backStyle: styles.correct,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'I',
+    front: 'R',
+    backStyle: styles.incorrect,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'N',
+    front: 'D',
+    backStyle: styles.misplaced,
+    frontStyle: styles.correct,
+  },
+  {
+    back: 'G',
+    front: 'L',
+    backStyle: styles.correct,
+    frontStyle: styles.correct,
+  },
+  {
+    back: '!',
+    front: 'E',
+    backStyle: styles.incorrect,
+    frontStyle: styles.correct,
+  },
+];
+
 const Loading = () => {
-  const styles = {
-    container: { height: '50px', width: '50px' },
-    correct: { backgroundColor: LETTER_STATUS.correct },
-    incorrect: { backgroundColor: LETTER_STATUS.incorrect },
-    misplaced: { backgroundColor: LETTER_STATUS.misplaced },
-  };
-
-  const loadingWords = [
-    {
-      back: 'L',
-      front: 'R',
-      backStyle: styles.correct,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'O',
-      front: 'S',
-      backStyle: styles.incorrect,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'A',
-      front: 'W',
-      backStyle: styles.misplaced,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'D',
-      front: 'O',
-      backStyle: styles.correct,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'I',
-      front: 'R',
-      backStyle: styles.incorrect,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'N',
-      front: 'D',
-      backStyle: styles.misplaced,
-      frontStyle: styles.correct,
-    },
-    {
-      back: 'G',
-      front: 'L',
-      backStyle: styles.correct,
-      frontStyle: styles.correct,
-    },
-    {
-      back: '!',
-      front: 'E',
-      backStyle: styles.incorrect,
-      frontStyle: styles.correct,
-    },
-  ];
-
   const [flipped, setFlipped] = useState(Array(loadingWords.length).fill(false));
   const [currentIndex, setCurrentIndex] = useState(0);
   const [timeoutRef, setTimeoutRef] = useState(null);
@@ -88,6 +88,7 @@ const Loading = () => {
     <div className="loading-container">
       {loadingWords.map(({ front, back, frontStyle, backStyle }, index) => (
         <ReactFlipCard
+          key={`${front}-${back}-${index}`}
           containerStyle={styles.container}
           flipTrigger="disabled"
           flipByProp={flipped[index]}

3a0a93fa4d862265666c58a9e079c44596be6431
Manuela Viola <manuela.viola@rootstrap.com>
2022-12-27 16:42:05 -0300

    fix: some select styles

--- src/components/common/Select/index.js
+++ src/components/common/Select/index.js
@@ -3,27 +3,47 @@ import SelectReact from 'react-select';
 
 import './styles.css';
 
-const backgroundColor = 'rgba(83, 141, 78, 0.85)';
+const activeColor = '#3d6b39';
+const backgroundColor = '#538d4e';
+const color = 'white';
+const focusedColor = '#3d6b39';
+const hoverColor = '#c6d4c5';
+const selectedColor = '#1f1f1f';
 
 const Select = ({ ...props }) => {
   const colorStyles = {
-    control: styles => ({
+    control: (styles, { isFocused }) => ({
       ...styles,
       backgroundColor: backgroundColor,
-      borderColor: backgroundColor,
+      borderColor: isFocused ? color : backgroundColor,
+      borderWidth: 2,
+
+      ':hover': {
+        ...styles[':hover'],
+        borderColor: color,
+      },
     }),
+    dropdownIndicator: styles => ({
+      ...styles,
+      color,
+
+      ':hover': {
+        ...styles[':hover'],
+        color: hoverColor,
+      },
+    }),
+    menu: styles => ({ ...styles, backgroundColor }),
     option: (styles, { isFocused, isSelected }) => ({
       ...styles,
-      backgroundColor: isSelected ? '#1f1f1f' : backgroundColor,
-      opacity: isFocused && !isSelected ? 0.7 : 1,
-      color: 'white',
+      backgroundColor: isSelected ? selectedColor : isFocused ? focusedColor : backgroundColor,
+      color,
 
       ':active': {
         ...styles[':active'],
-        backgroundColor: 'red',
+        backgroundColor: activeColor,
       },
     }),
-    singleValue: styles => ({ ...styles, color: 'white' }),
+    singleValue: styles => ({ ...styles, color }),
   };
   return <SelectReact {...props} styles={colorStyles} />;
 };

0ec276a968991d41342a58b4d0b6dd9ef16c19ee
Manuela Viola <manuela.viola@rootstrap.com>
2023-01-10 21:30:19 -0300

    fix: remove unnecessary fragment

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -20,30 +20,28 @@ const Statistics = () => {
   } = useUserStatistics(location?.state ?? {});
 
   return (
-    <>
-      <div className="statistics-container">
-        <h1 className="page-title">{`${userName} Statistics`}</h1>
-        <img src={profilePhoto} className="statistics-photo" alt={`user-${userName}`} />
-        {!!Object.keys(statistics).length && (
-          <>
-            <div className="stat-box-container">
-              <StatBox label="Played" value={totalGames} />
-              <StatBox label="Wins" value={totalWins} />
-              <StatBox
-                label="Wins (%)"
-                value={totalGames ? ((totalWins * 100) / totalGames).toFixed(0) : 0}
-              />
-              <StatBox label="Current Streak" value={currentStreak} />
-              <StatBox label="Longest Streak" value={longestStreak} />
-            </div>
-            <div className="statistics-charts-container">
-              <HorizontalBarChart data={totalAttempts} maxValue={maxAttemptsRound} />
-              <HorizontalBarChart data={topAttemptedWords} maxValue={maxAttemptedWords} words />
-            </div>
-          </>
-        )}
-      </div>
-    </>
+    <div className="statistics-container">
+      <h1 className="page-title">{`${userName} Statistics`}</h1>
+      <img src={profilePhoto} className="statistics-photo" alt={`user-${userName}`} />
+      {!!Object.keys(statistics).length && (
+        <>
+          <div className="stat-box-container">
+            <StatBox label="Played" value={totalGames} />
+            <StatBox label="Wins" value={totalWins} />
+            <StatBox
+              label="Wins (%)"
+              value={totalGames ? ((totalWins * 100) / totalGames).toFixed(0) : 0}
+            />
+            <StatBox label="Current Streak" value={currentStreak} />
+            <StatBox label="Longest Streak" value={longestStreak} />
+          </div>
+          <div className="statistics-charts-container">
+            <HorizontalBarChart data={totalAttempts} maxValue={maxAttemptsRound} />
+            <HorizontalBarChart data={topAttemptedWords} maxValue={maxAttemptedWords} words />
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 

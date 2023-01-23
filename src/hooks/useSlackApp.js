0d281168a12a9ba64e4a9ca219af12c764ceefe1
Manuela Viola <manuela.viola@rootstrap.com>
2023-01-23 19:41:58 -0300

    fix: remove conditional call of function findConversation

--- src/hooks/useSlackApp.js
+++ src/hooks/useSlackApp.js
@@ -1,38 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 import axios from 'axios';
 
 const SLACK_TOKEN = process.env.REACT_APP_SLACK_TOKEN;
 const CHANNEL_ID = process.env.REACT_APP_WORDLE_CHANNEL_ID;
 const TEST_CHANNEL_ID = process.env.REACT_APP_WORDLE_TEST_CHANNEL_ID;
 const USE_TEST_CHANNEL = process.env.REACT_APP_USE_TEST_CHANNEL;
-const EXCLUDE_ARCHIVED = true;
-const LIMIT = 700;
 
 const useSlackApp = () => {
-  // wordle channelId = C02U49H0VJA
-  // test-wordle channelId = C04KRFK4VJA
-  const [channelId, setChannelId] = useState(USE_TEST_CHANNEL ? TEST_CHANNEL_ID : CHANNEL_ID);
-  const callFindConversation = false;
+  const channelId = useMemo(() => (USE_TEST_CHANNEL ? TEST_CHANNEL_ID : CHANNEL_ID), []);
 
+  /*
+    The ID is always the same. I use this to get the id and I leave it in case the
+    channel changes, but right now is unnecesary to call it every time since
+    the id doesnt change and I already know it (C02U49H0VJA) 
+  */
+  // eslint-disable-next-line no-unused-vars
   const findConversation = async channelName => {
     try {
+      const EXCLUDE_ARCHIVED = true;
+      const LIMIT = 700;
       const body = `token=${SLACK_TOKEN}&exclude_archived=${EXCLUDE_ARCHIVED}&limit=${LIMIT}`;
       const { data } = await axios.post('https://slack.com/api/conversations.list', body);
 
       const { id: channelId } = data?.channels?.find(({ name }) => name === channelName) || {};
-      setChannelId(channelId);
+      console.log('channelId: ', channelId);
     } catch (error) {
       console.error(error);
     }
   };
-
-  useEffect(() => {
-    /*
-     The ID is always the same. I use this to get the id and I leave it in case the
-     channel changes, but right now is unnecesary to call it every time since
-     the id doesnt change and I already know it (C02U49H0VJA) */
-    callFindConversation && findConversation('wordle');
-  }, [callFindConversation]);
 
   const sendMessageToChannel = async message => {
     try {

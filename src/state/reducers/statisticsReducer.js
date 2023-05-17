21e289717ff0b2afe6818e8bf5d08c41379f82f7
Manuela Viola <manuela.viola@rootstrap.com>
2023-05-17 17:55:24 -0300

    fix: clean reducer data when the user logs out

--- src/state/reducers/statisticsReducer.js
+++ src/state/reducers/statisticsReducer.js
@@ -1,8 +1,9 @@
 import { createReducer } from '@rootstrap/redux-tools';
 
 import { setUserStatistics } from 'state/actions/statisticsActions';
+import { logout } from 'state/actions/userActions';
 
-const initialState = {
+export const initialState = {
   statistics: {},
 };
 
@@ -10,6 +11,11 @@ const handleSetUsersStatistics = (state, { payload: { statistics, selectedUser }
   state.statistics = { ...state.statistics, [selectedUser]: statistics };
 };
 
+const handleLogout = () => {
+  return initialState;
+};
+
 export default createReducer(initialState, {
+  [logout]: handleLogout,
   [setUserStatistics]: handleSetUsersStatistics,
 });
